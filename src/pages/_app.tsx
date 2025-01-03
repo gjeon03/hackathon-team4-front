f7a34524b2b9637aa9ab1d81b1c5a590a115331f
gyeongyeon <gjeon0321@gmail.com>
2025-01-04 01:40:03 +0900

    fix: start msw

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-if (process.env.NODE_ENV === "development") {
-  // require("../mocks");
+if (
+  process.env.NODE_ENV === "development" &&
+  process.env.NEXT_PUBLIC_USE_MOCKS === "true"
+) {
+  require("../mocks");
 }
 
 export default function App({ Component, pageProps }: AppProps) {
